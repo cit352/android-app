63b4143d1cc8f39b8f575b11dc7d93393b9d7c66
Mitchell B. Roe <mbroe@oakland.edu>
2016-10-14 18:56:22 -0400

    Add registration, login, and unlock start pages

---
+++ app/src/main/java/com/example/pete/myapplication/login.java
@@ -0,0 +1,39 @@
+package com.example.pete.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    public Button enter_app;
+
+    public void init(){
+        enter_app = (Button)findViewById(R.id.enter_app);
+        enter_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent tin = new Intent (login.this,unlockstart.class) ;
+                startActivity(tin);
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+        getSupportActionBar().setHomeButtonEnabled(true);//this makes back button go back to home
+    }
+}
+
+
+
